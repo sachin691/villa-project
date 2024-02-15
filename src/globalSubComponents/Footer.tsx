469dddc97d5744adfad7ff8b34b05febf4b74dcd
Sachin <sachinkaurav65@gmail.com>
2024-02-15 17:42:03 +0530

    add footer

--- src/globalSubComponents/Footer.tsx
+++ src/globalSubComponents/Footer.tsx
@@ -11,7 +11,7 @@ import { IoLocation } from "react-icons/io5";
 
 // Local Files
 import "./Footer.css";
-import logo from "../globalAssets/logo.svg";
+// import logo from "../../public/favicon.svg";
 
 const contactInfo = [
   {
@@ -35,63 +35,108 @@ const footerLinks = [
   {
     type: "Navigation",
     data: [
-      { key: "home", value: "Home", link: "Home", state: {} },
+      { key: "about", value: "About Us", link: "About-Us", state: {} },
+      { key: "careers", value: "Careers", link: "Careers", state: {} },
       { key: "services", value: "Services", link: "Services", state: {} },
-      { key: "about", value: "About", link: "About", state: {} },
-      { key: "contact", value: "Contact", link: "Contact", state: {} },
-      { key: "pricing", value: "Pricing", link: "Pricing", state: {} },
+      { key: "contact", value: "Contact Us", link: "Contact-Us", state: {} },
+      { key: "terms", value: "Terms & Conditions", link: "Terms-Conditions", state: {} },
+      { key: "policy", value: "Policy of Use", link: "Policy-of-Use", state: {} },
+      { key: "sitemap", value: "Sitemap", link: "Sitemap", state: {} },
+      { key: "blog", value: "Blog", link: "Blog", state: {} },
     ],
   },
   {
-    type: "Quick Links",
+    type: "Network Sites",
     data: [
-      { key: "webdev", value: "Website Development", link: "Services/Individual", state: { id: 0 } },
-      { key: "seo", value: "Search Engine Optimization", link: "Services/Individual", state: { id: 1 } },
-      { key: "socialmarketing", value: "Social Media Marketing", link: "Services/Individual", state: { id: 2 } },
-      { key: "webanalytics", value: "Web Analytics", link: "Services/Individual", state: { id: 3 } },
-      { key: "graphicdesign", value: "Graphics Design", link: "Services/Individual", state: { id: 4 } },
+      { key: "canada", value: "Villa Canada", link: "Villa-Canada", state: {} },
+      { key: "uae", value: "Villa UAE", link: "Villa-UAE", state: {} },
+      { key: "australia", value: "Villa Australia", link: "Villa-Australia", state: {} },
+      { key: "urbanmoney", value: "Urban Money", link: "Urban-Money", state: {} },
+      { key: "urbanmoneyaustralia", value: "Urban Money Australia", link: "Urban-Money-Australia", state: {} },
+      { key: "interiorcompany", value: "Interior Company", link: "Interior-Company", state: {} },
+      { key: "propvr", value: "PropVR", link: "PropVR", state: {} },
+      { key: "propsamc", value: "PropsAMC", link: "PropsAMC", state: {} },
+      { key: "azuro", value: "Azuro - Property Management", link: "Azuro-Property-Management", state: {} },
+    ],
+  },
+  {
+    type: "Solutions",
+    data: [
+      { key: "squareconnect", value: "Villa Connect", link: "Villa-Connect", state: {} },
+      { key: "primeconnect", value: "Prime Connect", link: "Prime-Connect", state: {} },
+      { key: "smartagent", value: "Smart Agent", link: "Smart-Agent", state: {} },
+      { key: "realizing", value: "Realizing", link: "Realizing", state: {} },
+      { key: "rentagreement", value: "Rent Agreement", link: "Rent-Agreement", state: {} },
+      { key: "escrow", value: "Escrow", link: "Escrow", state: {} },
+      { key: "bookpropertyonline", value: "Book Property Online", link: "Book-Property-Online", state: {} },
+    ],
+  },
+  {
+    type: "Resources",
+    data: [
+      { key: "dataintelligence", value: "Data Intelligence", link: "Data-Intelligence", state: {} },
+      { key: "villareviews", value: "Villa Reviews", link: "Villa-Reviews", state: {} },
+      { key: "realestateguide", value: "Real Estate Guide", link: "Real-Estate-Guide", state: {} },
+      { key: "citiesrealestate", value: "Cities Real Estate", link: "Cities-Real-Estate", state: {} },
+      { key: "awardsrecognition", value: "Awards & Recognition", link: "Awards-Recognition", state: {} },
+      { key: "mediacoverage", value: "Media Coverage", link: "Media-Coverage", state: {} },
+      { key: "glossary", value: "Glossary", link: "Glossary", state: {} },
     ],
   },
 ];
 
 const Footer = () => {
   return (
-    <div className="flex flex-col justify-center items-center py-[6rem] gap-[2rem] text-white footer">
-      <div className="flex gap-[4rem] upperFooter">
-        <div className="max-w-[20rem] flex flex-col gap-[2rem] companySection">
-          <Image width={150} src={logo} alt="logo" className="bg-white p-[1rem]" />
-          <p className="text-justify text-sm">
-            Kreative Machinez is a unit of <span className="font-bold">HMS Freedom LLC</span>, we're a lively digital
-            marketing agency that's all about crafting innovative solutions for businesses like yours, helping you build
-            a robust online presence that truly stands out. Driven by a ferocious hunger to create tangible impact for
-            your brands, we work with in-house specialists, industry partners and technology leaders to push the
-            boundaries of creativity and put your brand ahead of the competition.
-          </p>
-
-          <Link to='/Privacy' className="text-blue-400">Privacy & Policy</Link>
-          <div className="flex text-[1.8rem] gap-[1rem]">
-            <a href="https://www.facebook.com/profile.php?id=61554936182745" target="_blank" rel="noreferrer">
-              <SiFacebook className="mt-[0.4rem] cursor-pointer hover:scale-105" />
-            </a>
-            <a href="https://twitter.com/Kreativemachinz" target="_blank" rel="noreferrer">
-              <FaSquareXTwitter className="mt-[0.4rem] cursor-pointer hover:scale-105" />
-            </a>
-            <a href="https://www.youtube.com/@KreativeMachinez-23" target="_blank" rel="noreferrer">
-              <TbBrandYoutubeFilled className="mt-[0.4rem] cursor-pointer hover:scale-105" />
-            </a>
-            <a href="https://www.instagram.com/kreativemachinez23/" target="_blank" rel="noreferrer">
-              <AiFillInstagram className="mt-[0.4rem] cursor-pointer hover:scale-105" />
-            </a>
-            <a href="https://www.pinterest.com/KreativeMachinez23/" target="_blank" rel="noreferrer">
-              <FaPinterest className="mt-[0.4rem] cursor-pointer hover:scale-105" />
-            </a>
+    <div className="flex flex-col justify-center items-center py-[3rem] px-[4rem] gap-[2rem] text-black bg-white">
+      <div className="w-[100%]">
+        <Divider className="text-black" />
+        <Divider className="text-black" />
+        <Divider className="text-black" />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-x-[5rem]">
+        {footerLinks.map((ele, index) => (
+          <div key={index} className="flex flex-col md:gap-[1rem]">
+            <p className="font-['poppins'] text-[#F5A524] font-bold text-[1.1rem]">{ele.type}</p>
+            <Listbox items={ele.data} aria-label="navigation" className="dark" variant="light">
+              {(item) => (
+                <ListboxItem key={item.key} className="px-0" textValue={item.key}>
+                  <Link to={`../${item.link}`} state={item.state} className="hover:text-[#F5A524]">
+                    {item.value}
+                  </Link>
+                </ListboxItem>
+              )}
+            </Listbox>
           </div>
-        </div>
+        ))}
+      </div>
+      <div className="w-[100%]">
+        <Divider className="text-black" />
+        <Divider className="text-black" />
+        <Divider className="text-black" />
+      </div>
 
-        <div className="flex flex-col items-center justify-center gap-[1.2rem]">
-          <div className="flex gap-[2rem] w-full justify-between contactInfo">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-start lg:items-center md:px-[4rem] gap-[4rem]">
+        <div className="">
+          <h1 className="text-xl font-bold text-[#F5A524] text-center md:text-start">About us</h1>
+          <p className="text-center md:text-start">
+            Villa.com is your premier destination for exquisite villa rentals and vacation experiences worldwide. We
+            specialize in curating unforgettable stays in luxurious villas, offering a handpicked selection of
+            properties that cater to discerning travelers seeking privacy, comfort, and unparalleled hospitality. With a
+            focus on delivering exceptional villa experiences, Villa.com provides a seamless booking platform where
+            guests can discover and reserve their dream vacation home with ease. Whether you're planning a romantic
+            getaway, a family reunion, or a group retreat, our portfolio of stunning villas ensures that every stay is
+            nothing short of extraordinary. Committed to exceeding our guests' expectations, Villa.com combines
+            personalized service with cutting-edge technology to enhance every aspect of the booking process. Our team
+            of dedicated professionals is passionate about creating unforgettable memories for our guests, ensuring that
+            each villa experience is tailored to your unique preferences and needs. Experience the epitome of luxury and
+            relaxation with Villa.com. Discover your perfect villa retreat and embark on a journey of unparalleled
+            comfort and indulgence.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex flex-col gap-[2rem] contactInfo">
             {contactInfo.map((data, index) => (
-              <div key={index} className="flex items-center  gap-[1rem]">
+              <div key={index} className="flex items-center gap-[1rem]">
                 <Button isIconOnly variant="solid" radius="full" className="w-[3rem] h-[3rem] text-[1.5rem]">
                   {data.icon}
                 </Button>
@@ -102,49 +147,33 @@ const Footer = () => {
               </div>
             ))}
           </div>
-
-          <div className="w-[100%] upperDivider">
-            <Divider className="dark" />
-            <Divider className="dark" />
-          </div>
-
-          <div className="flex justify-between w-full footerLinks">
-            {footerLinks.map((ele, index) => (
-              <div key={index} className="flex flex-col gap-[1rem]">
-                <p className="font-['poppins'] text-[#F5A524] font-bold text-[1.1rem]">{ele.type}</p>
-                <Listbox items={ele.data} aria-label="navigation" className="dark" variant="light">
-                  {(item) => (
-                    <ListboxItem key={item.key} className="px-[0]" textValue={item.key}>
-                      <Link to={`../${item.link}`} state={item.state}>
-                        {item.value}
-                      </Link>
-                    </ListboxItem>
-                  )}
-                </Listbox>
-              </div>
-            ))}
-
-            <div className="flex flex-col gap-[1rem] max-w-[13rem]">
-              <p className="font-['poppins'] text-[#F5A524] font-bold text-[1.1rem]">Work Hours</p>
-              <p className="text-justify text-sm">
-                Feel free to reach out to us during our working hours for all your digital marketing needs. We're here
-                to elevate your online presence and drive success for your business.
-              </p>
-              <ul className="text-sm">
-                <li>Mon - Fri : 09:00 - 18:00 (GMT)</li>
-                <li>Sat: 10:00 - 16:00 (GMT)</li>
-              </ul>
+          <div className="flex flex-col text-[2rem] gap-[1rem] p-[1rem] ">
+            <h1 className="text-xl font-bold text-[#F5A524]">Find us Here</h1>
+            <div className="flex flex-row gap-[2rem]">
+              <a href="https://www.facebook.com/profile.php?id=61554936182745" target="_blank" rel="noreferrer">
+                <SiFacebook className="mt-[0.4rem] cursor-pointer hover:scale-105" />
+              </a>
+              <a href="https://twitter.com/Kreativemachinz" target="_blank" rel="noreferrer">
+                <FaSquareXTwitter className="mt-[0.4rem] cursor-pointer hover:scale-105" />
+              </a>
+            </div>
+            <div className="flex flex-row gap-[2rem]">
+              <a href="https://www.youtube.com/@KreativeMachinez-23" target="_blank" rel="noreferrer">
+                <TbBrandYoutubeFilled className="mt-[0.4rem] cursor-pointer hover:scale-105" />
+              </a>
+              <a href="https://www.instagram.com/kreativemachinez23/" target="_blank" rel="noreferrer">
+                <AiFillInstagram className="mt-[0.4rem] cursor-pointer hover:scale-105" />
+              </a>
             </div>
           </div>
         </div>
       </div>
       <div className="w-[100%]">
-        <Divider className="dark" />
-        <Divider className="dark" />
-        <Divider className="dark" />
+        <Divider className="text-black" />
+        <Divider className="text-black" />
+        <Divider className="text-black" />
       </div>
-
-      <p>© 2023 Kreative Machinez • All Rights Reserved</p>
+      <p>© 2024 Villa • All Rights Reserved</p>
     </div>
   );
 };
